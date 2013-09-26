62f1a577939a9b8f43abf29b8a692eed2877fb28
hanse <thehanse@gmail.com>
2013-09-26 10:13:06 +0200

    fixed compatibatility issues

--- ReservationDatabase.hs
+++ ReservationDatabase.hs
@@ -23,7 +23,7 @@ module ReservationDatabase (StationId, TrainId,
                             makeReservation
                             ) where
 
-import Data.Map hiding (map, filter, foldl)
+import Data.Map hiding (map, filter, foldr)
 import qualified Data.Map as M (filter)
 import qualified Data.Set as S
 import qualified Data.Foldable as GenFold
